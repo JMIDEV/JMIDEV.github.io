53082af76593bf8cfb92186cced1af986e9bb2c0
JMI121GYHU <53763238+JMI121GYHU@users.noreply.github.com>
2020-09-11 13:02:27 +0200

    First Update

---
+++ topbar.js
@@ -0,0 +1,23 @@
+function goInicio(){
+
+    window.location.href = "index.html";
+
+}
+
+function goProyectos(){
+
+    window.location.href = "mis-proyectos.html";
+
+}
+
+function goRedes(){
+
+    window.location.href = "redes-sociales.html";
+
+}
+
+function goBlog(){
+
+    window.location.href = "blog.html";
+
+}
