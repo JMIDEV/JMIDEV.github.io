9c46ef7a725fcbb61bfcd2601690a660213d0111
JMIDEV <53763238+JMIDEV@users.noreply.github.com>
2020-12-26 17:25:04 +0100

    MY PROJECTS UPDATE 2.1

---
+++ projectview.js
@@ -0,0 +1,489 @@
+//JSON --------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+var projectsdata = {
+
+    "retimethewind": {
+
+        "projname": "Retime the Wind",
+        "shortdescription": "En Retime the Wind puedes rebobinar el tiempo con una cinta de cassete.",
+        "description": "En Retime the Wind, eres un cocodrilo que tiene que comer bolas de chicle rebobinando el tiempo, para que la gente no flote, y para satisfacer su apetito. (Participó en la BRACKEYS GAME JAM)",
+        "authors": "JMIDEV",
+        "rating": 4,
+        "photos": {
+
+            "cover": "FotosDeProyectos/GAME JAMS/Retime the Wind/RTW.jpg",
+            "photo1": "FotosDeProyectos/GAME JAMS/Retime the Wind/CAPT1.png",
+            "photo2": "FotosDeProyectos/GAME JAMS/Retime the Wind/CAPT2.png",
+            "photo3": "FotosDeProyectos/GAME JAMS/Retime the Wind/CAPT3.png"
+
+        },
+        "platforms": {
+
+            "itchio": "https://jmidev.itch.io/retime-the-wind",
+            /*"mega": "jeje",
+            "web": "jojo",
+            "construct": "jiji"*/
+
+        }
+
+    },
+
+    "worldlifedemo": {
+
+        "projname": "World Life DEMO",
+        "shortdescription": "En esta aventura, Steven descubre una casa enorme, y decide explorarla.",
+        "description": "En esta aventura, Steven va a visitar a un conocido a su casa, la cual, al entrar, se vuelve enorme, por lo cual Steven decide explorarla y ver que hay detrás de este suceso.",
+        "authors": "JMIDEV (GYHU)",
+        "rating": 4.5,
+        "photos": {
+
+            "cover": "FotosDeProyectos/Aventura/worldlifedemo/COVER.png",
+            "photo1": "FotosDeProyectos/Aventura/worldlifedemo/capt1.png",
+            "photo2": "FotosDeProyectos/Aventura/worldlifedemo/capt2.png",
+            "photo3": "FotosDeProyectos/Aventura/worldlifedemo/capt3.png"
+
+        },
+        "platforms": {
+
+            "itchio": "https://jmidev.itch.io/world-life-demo",
+            /*"mega": "jeje",
+            "web": "jojo",*/
+            "construct": "https://www.construct.net/en/free-online-games/world-life-demo-12747/play"
+
+        }
+
+    },
+
+    "worldlifealpha": {
+
+        "projname": "World Life ALPHA",
+        "shortdescription": "(VERSIÓN INCOMPLETA DE WORLD LIFE DEMO)",
+        "description": "(VERSIÓN INCOMPLETA DE WORLD LIFE DEMO) Jugad mejor a la demo :)",
+        "authors": "JMIDEV (GYHU)",
+        "rating": 3.5,
+        "photos": {
+
+            "cover": "FotosDeProyectos/Aventura/World Life Alpha/COVER.png",
+            "photo1": "FotosDeProyectos/Aventura/worldlifedemo/capt1.png",
+            "photo2": "FotosDeProyectos/Aventura/worldlifedemo/capt2.png",
+            "photo3": "FotosDeProyectos/Aventura/worldlifedemo/capt3.png"
+
+        },
+        "platforms": {
+
+            //"itchio": "https://jmidev.itch.io/world-life-demo",
+            "mega": "https://mega.nz/file/yJITTajZ#122Q5hnbYlKQRlydu9T6L-XhiNYnq3EPDcmERIkj_Xk",
+            //"web": "jojo",
+            //"construct": "https://www.construct.net/en/free-online-games/world-life-demo-12747/play"
+
+        }
+
+    },
+
+    "rollingball": {
+
+        "projname": "Rolling Ball",
+        "shortdescription": "Transporta una bola hacia un cubo naranja para ganar.",
+        "description": "El reto de este juego, es transportar una bola/pelota mediante plataformas rotatorias que tu deberás controlar para llegar a la meta",
+        "authors": "JMIDEV (GYHU)",
+        "rating": 4,
+        "photos": {
+
+            "cover": "FotosDeProyectos/Casuales/Rolling Ball/COVER.jpg",
+            "photo1": "FotosDeProyectos/Casuales/Rolling Ball/CAPT1.jpg",
+            "photo2": "FotosDeProyectos/Casuales/Rolling Ball/CAPT2.jpg",
+            "photo3": "FotosDeProyectos/Casuales/Rolling Ball/CAPT3.jpg"
+
+        },
+        "platforms": {
+
+            //"itchio": "https://jmidev.itch.io/world-life-demo",
+            "mega": "https://mega.nz/file/GBJlWaKT#mQlyT3GqSsl8G0K1UdzZriIstHu5rcjzHxksOwFAGSA",
+            //"web": "jojo",
+            //"construct": "https://www.construct.net/en/free-online-games/world-life-demo-12747/play"
+
+        }
+
+    },
+
+    "rodantball": {
+
+        "projname": "Rodant Ball",
+        "shortdescription": "(CANCELADO) Este juego iba a ser el Remake de Rolling Ball.",
+        "description": "(CANCELADO) Este juego iba a ser el Remake de Rolling Ball. Fue cancelado debido a que estaba ocupado :(. Igualmente, está disponible una versión medio hecha.",
+        "authors": "JMIDEV",
+        "rating": 3.5,
+        "photos": {
+
+            "cover": "FotosDeProyectos/Casuales/Rodant Ball/COVER.jpg",
+            "photo1": "FotosDeProyectos/Casuales/Rodant Ball/CAPT1.jpg",
+            "photo2": "FotosDeProyectos/Casuales/Rodant Ball/CAPT2.jpg",
+            "photo3": "FotosDeProyectos/Casuales/Rodant Ball/CAPT3.jpg"
+
+        },
+        "platforms": {
+
+            //"itchio": "https://jmidev.itch.io/world-life-demo",
+            //"mega": "https://mega.nz/file/GBJlWaKT#mQlyT3GqSsl8G0K1UdzZriIstHu5rcjzHxksOwFAGSA",
+            "web": "https://jmidev.github.io/Rodant-Ball/",
+            //"construct": "https://www.construct.net/en/free-online-games/world-life-demo-12747/play"
+
+        }
+
+    }
+
+};
+
+
+//CODE --------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+var mainroot;
+
+var nombre;
+var description;
+var authors;
+var rating;
+var coverphoto;
+var photo1;
+var photo2;
+var photo3;
+var itchioavailable;
+var webavailable;
+var megaavailable;
+var constructavailable;
+
+//retrieve the url data
+
+const queryString = window.location.search;
+
+const urlParams = new URLSearchParams(queryString);
+
+mainroot = urlParams.get('proj');
+
+//read data from data obj
+
+nombre = projectsdata[mainroot].projname;
+description = projectsdata[mainroot].description;
+authors = projectsdata[mainroot].authors;
+rating = projectsdata[mainroot].rating;
+
+coverphoto = projectsdata[mainroot]["photos"].cover;
+photo1 = projectsdata[mainroot]["photos"].photo1;
+photo2 = projectsdata[mainroot]["photos"].photo2;
+photo3 = projectsdata[mainroot]["photos"].photo3;
+
+document.title = "JMIDEV - " + nombre;
+
+//write data to html tags
+
+document.getElementById("projectviewtitle").innerHTML = nombre;
+document.getElementById("projectviewdescription").innerHTML = description;
+document.getElementById("authorstext").innerHTML = authors;
+
+//
+//
+//write RATINGS
+
+if(rating == 1){
+
+    document.getElementById('star1').src="RateICONS/COMPLETE.svg";
+    document.getElementById('star2').src="RateICONS/EMPTY.svg";
+    document.getElementById('star3').src="RateICONS/EMPTY.svg";
+    document.getElementById('star4').src="RateICONS/EMPTY.svg";
+    document.getElementById('star5').src="RateICONS/EMPTY.svg";
+
+}
+
+if(rating == 1.5){
+
+    document.getElementById('star1').src="RateICONS/COMPLETE.svg";
+    document.getElementById('star2').src="RateICONS/MEDIUM.svg";
+    document.getElementById('star3').src="RateICONS/EMPTY.svg";
+    document.getElementById('star4').src="RateICONS/EMPTY.svg";
+    document.getElementById('star5').src="RateICONS/EMPTY.svg";
+
+}
+
+if(rating == 2){
+
+    document.getElementById('star1').src="RateICONS/COMPLETE.svg";
+    document.getElementById('star2').src="RateICONS/COMPLETE.svg";
+    document.getElementById('star3').src="RateICONS/EMPTY.svg";
+    document.getElementById('star4').src="RateICONS/EMPTY.svg";
+    document.getElementById('star5').src="RateICONS/EMPTY.svg";
+
+}
+
+if(rating == 2.5){
+
+    document.getElementById('star1').src="RateICONS/COMPLETE.svg";
+    document.getElementById('star2').src="RateICONS/COMPLETE.svg";
+    document.getElementById('star3').src="RateICONS/MEDIUM.svg";
+    document.getElementById('star4').src="RateICONS/EMPTY.svg";
+    document.getElementById('star5').src="RateICONS/EMPTY.svg";
+
+}
+
+if(rating == 3){
+
+    document.getElementById('star1').src="RateICONS/COMPLETE.svg";
+    document.getElementById('star2').src="RateICONS/COMPLETE.svg";
+    document.getElementById('star3').src="RateICONS/COMPLETE.svg";
+    document.getElementById('star4').src="RateICONS/EMPTY.svg";
+    document.getElementById('star5').src="RateICONS/EMPTY.svg";
+
+}
+
+if(rating == 3.5){
+
+    document.getElementById('star1').src="RateICONS/COMPLETE.svg";
+    document.getElementById('star2').src="RateICONS/COMPLETE.svg";
+    document.getElementById('star3').src="RateICONS/COMPLETE.svg";
+    document.getElementById('star4').src="RateICONS/MEDIUM.svg";
+    document.getElementById('star5').src="RateICONS/EMPTY.svg";
+
+}
+
+if(rating == 4){
+
+    document.getElementById('star1').src="RateICONS/COMPLETE.svg";
+    document.getElementById('star2').src="RateICONS/COMPLETE.svg";
+    document.getElementById('star3').src="RateICONS/COMPLETE.svg";
+    document.getElementById('star4').src="RateICONS/COMPLETE.svg";
+    document.getElementById('star5').src="RateICONS/EMPTY.svg";
+
+}
+
+if(rating == 4.5){
+
+    document.getElementById('star1').src="RateICONS/COMPLETE.svg";
+    document.getElementById('star2').src="RateICONS/COMPLETE.svg";
+    document.getElementById('star3').src="RateICONS/COMPLETE.svg";
+    document.getElementById('star4').src="RateICONS/COMPLETE.svg";
+    document.getElementById('star5').src="RateICONS/MEDIUM.svg";
+
+}
+
+if(rating == 5){
+
+    document.getElementById('star1').src="RateICONS/COMPLETE.svg";
+    document.getElementById('star2').src="RateICONS/COMPLETE.svg";
+    document.getElementById('star3').src="RateICONS/COMPLETE.svg";
+    document.getElementById('star4').src="RateICONS/COMPLETE.svg";
+    document.getElementById('star5').src="RateICONS/COMPLETE.svg";
+
+}
+
+//
+//
+//CAPTURES WORKING ---------------------------------------------------
+
+var currentcapture = 0;
+
+document.getElementById("coverfoto").style.backgroundImage = "url('" + coverphoto + "')";
+document.getElementById("photofoto1").style.backgroundImage = "url('" + photo1 + "')";
+document.getElementById("photofoto2").style.backgroundImage = "url('" + photo2 + "')";
+document.getElementById("photofoto3").style.backgroundImage = "url('" + photo3 + "')";
+$(".prevfotobutton").css("background-size", "cover");
+$(".prevfotobutton").css("background-repeat", "no-repeat");
+$(".prevfotobutton").css("background-position", "center");
+
+function checkcurrentcapt(){
+
+    if(currentcapture == 0){
+
+        document.getElementById('currentprevfoto').src=coverphoto;
+        $("#coverfoto").css("border", "5px solid rgb(91, 134, 255)");
+        $("#photofoto1").css("border", "none");
+        $("#photofoto2").css("border", "none");
+        $("#photofoto3").css("border", "none");
+
+    }
+    
+    if(currentcapture == 1){
+    
+        document.getElementById('currentprevfoto').src=photo1;
+        $("#coverfoto").css("border", "none");
+        $("#photofoto1").css("border", "5px solid rgb(91, 134, 255)");
+        $("#photofoto2").css("border", "none");
+        $("#photofoto3").css("border", "none");
+    
+    }
+    
+    if(currentcapture == 2){
+    
+        document.getElementById('currentprevfoto').src=photo2;
+        $("#coverfoto").css("border", "none");
+        $("#photofoto1").css("border", "none");
+        $("#photofoto2").css("border", "5px solid rgb(91, 134, 255)");
+        $("#photofoto3").css("border", "none");
+    
+    }
+    
+    if(currentcapture == 3){
+    
+        document.getElementById('currentprevfoto').src=photo3;
+        $("#coverfoto").css("border", "none");
+        $("#photofoto1").css("border", "none");
+        $("#photofoto2").css("border", "none");
+        $("#photofoto3").css("border", "5px solid rgb(91, 134, 255)");
+    
+    }
+
+}
+
+checkcurrentcapt();
+
+function changecapture(captnumber){
+
+    currentcapture = captnumber;
+
+    checkcurrentcapt();
+
+}
+
+//set available platforms
+
+
+var getgetpageopened = false;
+
+function checkifgetgetopened(){
+
+    if(getgetpageopened){
+
+        $(".getprojscreencontainer").css("display", "inline");
+
+    }
+
+    else{
+
+        $(".getprojscreencontainer").css("display", "none");
+
+    }
+
+}
+
+checkifgetgetopened();
+
+function changegetgetpageopened(opened){
+
+    getgetpageopened = opened;
+    
+    checkifgetgetopened();
+
+}
+
+
+
+
+//itchio
+
+if(projectsdata[mainroot]["platforms"].itchio == null){
+
+    itchioavailable = false;
+
+}
+
+else{
+
+    itchioavailable = true;
+
+}
+
+//mega
+
+if(projectsdata[mainroot]["platforms"].mega == null){
+
+    megaavailable = false;
+
+}
+
+else{
+
+    megaavailable = true;
+
+}
+
+//web
+
+if(projectsdata[mainroot]["platforms"].web == null){
+
+    webavailable = false;
+
+}
+
+else{
+
+    webavailable = true;
+
+}
+
+//construct
+
+if(projectsdata[mainroot]["platforms"].construct == null){
+
+    constructavailable = false;
+
+}
+
+else{
+
+    constructavailable = true;
+
+}
+
+//show existent platforms
+
+if(itchioavailable){
+
+    $("#getprojectfromitch").css("display", "inline");
+    document.getElementById('getprojectfromitch').href=projectsdata[mainroot]["platforms"].itchio;
+
+
+}
+
+else{
+
+    $("#getprojectfromitch").css("display", "none");
+
+}
+
+if(megaavailable){
+
+    $("#getprojectfrommega").css("display", "inline");
+    document.getElementById('getprojectfrommega').href=projectsdata[mainroot]["platforms"].mega;
+
+}
+
+else{
+
+    $("#getprojectfrommega").css("display", "none");
+
+}
+
+if(webavailable){
+
+    $("#getprojectfromweb").css("display", "inline");
+    document.getElementById('getprojectfromweb').href=projectsdata[mainroot]["platforms"].web;
+
+}
+
+else{
+
+    $("#getprojectfromweb").css("display", "none");
+
+}
+
+if(constructavailable){
+
+    $("#getprojectfromconstruct").css("display", "inline");
+    document.getElementById('getprojectfromconstruct').href=projectsdata[mainroot]["platforms"].construct;
+
+}
+
+else{
+
+    $("#getprojectfromconstruct").css("display", "none");
+
+}
